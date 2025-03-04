8c1abd549ba98a48b7a949e4987482934fc49d27
ilaydagiris <101815936+ilaydagiris@users.noreply.github.com>
2025-03-04 23:37:17 +0200

    Update Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  distDir: 'dist',
+  basePath: '',
   images: {
-    unoptimized: true,
-    loader: 'custom',
-    loaderFile: './src/app/image.js',
+    unoptimized: true
   }
 };
 
