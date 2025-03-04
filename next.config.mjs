279c40cd314452a4a38a284a254df57c4a29145e
ilaydagiris <101815936+ilaydagiris@users.noreply.github.com>
2025-03-04 23:32:44 +0200

    Update Next.js configuration for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '',
   images: {
     unoptimized: true
-  }
+  },
+  trailingSlash: true
 };
 
 export default nextConfig;
